4c78ec7642c78c0e692d83b60aa2f538cb5269a6
JorgeToT <jorge.agvel@gmail.com>
2025-05-22 04:31:29 -0600

    feat: update styles and structure for some ui components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,31 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+type InputProps = React.ComponentProps<'input'> & {
+  icon?: React.ReactNode;
+};
+
+function Input({ className, type, icon, ...props }: InputProps) {
+  const hasIcon = Boolean(icon);
+  const paddingClass = hasIcon ? 'pr-10' : '';
+
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
+    <div className={cn('relative inline-flex w-full', className)}>
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          paddingClass,
+          'file:text-foreground placeholder:text-secondary-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex w-full min-w-0 rounded-lg border bg-transparent px-4 py-2.5 shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 text-[14px]/5 md:text-sm',
+          'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[2px]',
+          'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+        )}
+        {...props}
+      />
+      {icon && icon}
+    </div>
+  );
 }
 
-export { Input }
+export { Input };
