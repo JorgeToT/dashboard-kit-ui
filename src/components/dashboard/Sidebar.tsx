ff81aa3fa821363935e4f177548bbabbf97b1db5
JorgeToT <jorge.agvel@gmail.com>
2025-05-24 16:10:04 -0600

    feat: update sidebar and header components for improved mobile responsiveness

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -8,25 +8,44 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarHeader,
+  useSidebar,
 } from '@/components/ui/sidebar';
 import { Separator } from '@/components/ui/separator';
 import Image from 'next/image';
 import SidebarMenuGroup from './SidebarMenuGroup';
 import { items } from './SidebarItems';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 const group1 = items.slice(0, 6);
 const group2 = items.slice(6);
 
 const Sidebar = () => {
   const [activeTitle, setActiveTitle] = useState(items[0].title);
+  const { toggleSidebar, openMobile } = useSidebar();
+  const isMobile = useIsMobile();
+
+  const handleSidebarToggle = () => {
+    if (isMobile) {
+      toggleSidebar();
+    }
+  };
 
   return (
     <UISidebar>
-      <SidebarHeader className="flex flex-row gap-3 pt-[37px] pl-8 pb-0 items-center mb-[59px]">
-        <Image src={logoIcon} alt="Logo" width={32} height={32} />
-        <h1 className="text-[19px] tracking-[0.4px] font-bold">
-          Dashboard Kit
-        </h1>
+      <SidebarHeader className="flex flex-row gap-3 pt-[37px] pl-5 md:pl-8 pb-0 items-center mb-[59px]">
+        <Image
+          src={logoIcon}
+          alt="Logo"
+          width={32}
+          height={32}
+          onClick={handleSidebarToggle}
+          className="cursor-pointer md:cursor-auto"
+        />
+        {((isMobile && openMobile) || !isMobile) && (
+          <h1 className="text-[19px] tracking-[0.4px] font-bold">
+            Dashboard Kit
+          </h1>
+        )}
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
